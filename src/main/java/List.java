7aa369e1e201d8e33d49dc5681c7af0637f2fc74
Sebastian Österlund <s.osterlund@vu.nl>
2016-09-19 08:56:45 +0200

    Initial Commit

---
+++ src/main/java/List.java
@@ -0,0 +1,80 @@
+public class List<E extends Data<E>> implements ListInterface<E>{
+
+    class Node {
+
+        E data;
+        Node prior,
+                next;
+
+        public Node(E d) {
+            this(d, null, null);
+        }
+
+        public Node(E data, Node prior, Node next) {
+            this.data = data == null ? null : data;
+            this.prior = prior;
+            this.next = next;
+        }
+
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public ListInterface<E> init() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public ListInterface<E> insert(E d) {
+        return null;
+    }
+
+    @Override
+    public E retrieve() {
+        return null;
+    }
+
+    @Override
+    public ListInterface<E> remove() {
+        return null;
+    }
+
+    @Override
+    public boolean find(E d) {
+        return false;
+    }
+
+    @Override
+    public boolean goToFirst() {
+        return false;
+    }
+
+    @Override
+    public boolean goToLast() {
+        return false;
+    }
+
+    @Override
+    public boolean goToNext() {
+        return false;
+    }
+
+    @Override
+    public boolean goToPrevious() {
+        return false;
+    }
+
+    @Override
+    public ListInterface<E> clone() {
+        return null;
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+
+    private void start() {
+        // Create a scanner on System.in
+        
+        // While there is input, read line and parse it.
+    }
+
+    public static void main(String[] argv) {
+        new Main().start();
+    }
+}
